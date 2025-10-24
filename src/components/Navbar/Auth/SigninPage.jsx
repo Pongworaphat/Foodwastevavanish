9306b8170e9f01d9bb62b32fbd58ee2c3e2a3d56
Pongworaphat <pongworaphat.i@ku.th>
2025-10-24 14:01:19 +0700

    Update Frontend

---
+++ src/components/Navbar/Auth/SigninPage.jsx
@@ -0,0 +1,3 @@
+export default function SigninPage() {
+  return <h1 className="text-center mt-10 text-2xl font-bold">Sign in Page</h1>;
+}
---
+++ src/components/Navbar/Auth/SignupPage.jsx
@@ -0,0 +1,3 @@
+export default function SignupPage() {
+  return <h1 className="text-center mt-10 text-2xl font-bold">Signup Page</h1>;
+}
